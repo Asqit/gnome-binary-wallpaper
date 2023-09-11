2adf472c292eaec032fddaf3a0b3125249b60814
Ondřej Tuček <ondrejtucek9@gmail.com>
2023-09-11 22:55:46 +0200

    TextUiController
    As for now I decided to create super simple textUI without any console arguments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        new AppController().createWallpaper("Forza", "/home/andrew/Downloads/m4.png", "/home/andrew/Downloads/e34-back.png");
+        TextUIController textUIController = new TextUIController();
+
+        textUIController.listen();
     }
 }
--- src/TextUIController.java
+++ src/TextUIController.java
@@ -1,64 +1,87 @@
-import org.apache.commons.cli.CommandLine;
+ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 
+import java.util.Scanner;
+
 public class TextUIController {
-    private final Options options = new Options();
-    private CommandLine cmd;
+   private final Scanner scanner = new Scanner(System.in);
 
-    public TextUIController(String[] args) {
-        try {
-            CommandLineParser parser = new DefaultParser();
-            this.cmd = parser.parse(this.options, args);
-        } catch (Exception error) {
-            System.out.println("Error: " + error.getMessage());
-        }
+   private void cls() {
+       for (int i = 0;i < 30;i++) {
+           System.out.println();
+       }
+   }
 
-        this.appendOptions();
-        this.parseArguments();
+   private void printHelp() {
+       this.cls();
+       System.out.println("┌────────────────────────────────────────────────┐");
+       System.out.println("│ Help                                           │");
+       System.out.println("│ ══════════════════════════════════════════════ │");
+       System.out.println("│ 1. create - starts creating the wallpaper      │");
+       System.out.println("│ 2. clear - clears your screen                  │");
+       System.out.println("│ 3. help - prints this table                    │");
+       System.out.println("│ 4. exit - quits the application                │");
+       System.out.println("└────────────────────────────────────────────────┘");
+   }
+
+    private void printGreetings() {
+        System.out.println("┌─────────────────────────────────────────────────────────────┐");
+        System.out.println("│ Gnome-binary-wallpaper                                      │");
+        System.out.println("│ ═══════════════════════════════════════════════════════════ │");
+        System.out.println("│ Create light & dark wallpapers with ease                    │");
+        System.out.println("│ an app by Ondřej Tuček                                      │");
+        System.out.println("│ see more at https://github.com/asqit/gnome-binary-wallpaper │");
+        System.out.println("└─────────────────────────────────────────────────────────────┘");
     }
 
+    private void createWallpaper() {
+        System.out.print("Wallpaper name:");
+        String name = this.scanner.nextLine();
 
-    private void printHelp() {
-        System.out.println("+---------------------------------------+");
-        System.out.println("| Gnome-Binary-Wallpaper                |");
-        System.out.println("+---------------------------------------+");
-        System.out.println("| utility for creating dark & light     |");
-        System.out.println("| wallpapers for gnome                  |");
-        System.out.println("+---------------------------------------+");
-        System.out.println("| 1. --help (-h) prints this message    |");
-        System.out.println("| 2. Example:                           |");
-        System.out.println("./gnome-binary-wallpaper -n 'dome' -dw '~/Downloads/dome-day.jpg' -nw '~/Download/dome-night.jpg'");
+        System.out.print("path to light wallpaper:");
+        String dayWallpaperPath = this.scanner.nextLine();
+
+        System.out.print("path to dark wallpaper:");
+        String nightWallpaperPath = this.scanner.nextLine();
+
+        AppController appController = new AppController();
+
+        appController.createWallpaper(name, dayWallpaperPath, nightWallpaperPath);
+        System.out.println("Wallpaper " + name + " has been successfully created!");
     }
 
-    private void appendOptions() {
-        this.options.addOption("h", "help", false, "Show help");
-        this.options.addOption("n", "name", true, "name of the wallpaper");
-        this.options.addOption("dw", "day-wallpaper", true, "wallpaper for day");
-        this.options.addOption("nw", "night-wallpaper", true, "wallpaper for night");
+    private void runner() {
+       while (true) {
+           System.out.print("command: ");
+           String input = this.scanner.nextLine();
+
+           if (input.equals("exit")) {
+               break;
+           }
+
+           if (input.equals("help") || input.equals("-h") || input.equals("--help")) {
+               this.printHelp();
+               continue;
+           }
+
+           if (input.equals("create")) {
+               this.createWallpaper();
+               continue;
+           }
+
+           if (input.equals("clear")) {
+               this.cls();
+               continue;
+           }
+
+           System.out.println("Unknown operand: " + input);
+       }
     }
 
-    private void parseArguments() {
-        if (this.cmd.hasOption("h")) {
-            this.printHelp();
-        }
-
-        if (this.cmd.hasOption('h')){
-            this.printHelp();
-        }
-
-        if (cmd.hasOption("n") && cmd.hasOption("dw") && cmd.hasOption("nw")) {
-            String wallpaperName = cmd.getOptionValue("n");
-            String dayWallpaperPath = cmd.getOptionValue("dw");
-            String nightWallpaperPath = cmd.getOptionValue("nw");
-
-            new AppController().createWallpaper(wallpaperName, dayWallpaperPath, nightWallpaperPath);
-        } else {
-            System.out.println("Invalid arguments. Use h for help.");
-            for (String arg : this.cmd.getArgs()) {
-                System.out.println(arg);
-            }
-        }
-    }
+   public void listen() {
+        this.printGreetings();
+        this.runner();
+   }
 }
